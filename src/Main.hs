68b0fff2e4be82226b44507d4c0648dc83aed9b2
kani <kani@nsi.co.jp>
2015-05-18 02:17:04 -0700

    Improve by adding type-safe URL

--- src/Main.hs
+++ src/Main.hs
@@ -21,13 +21,18 @@ renderMyRoute Home = "/"
 renderMyRoute About = "/about"
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|<h1>Hello World!<br>
-            <p>sample page<br>
-            <a href="/about">about
-            |]
+getHomeR = defaultLayout [whamlet|
+        <h1>Hello World!
+        <p>sample page
+        <a href="@{AboutR}">about
+    |]
 
 getAboutR :: Handler Html
-getAboutR = defaultLayout [whamlet|<h1>sample page|]
+getAboutR = defaultLayout [whamlet|
+        <h1>About!
+        <p>sample page
+        <a href="@{HomeR}">home
+    |]
 
 main :: IO ()
 main = warpEnv HelloWorld
